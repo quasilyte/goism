33ccb91c7142df312b7775797cd22c83468c4671
quasilyte <quasilyte@gmail.com>
2017-05-06 16:31:49 +0300

    type convertion intrinsics (Int, String, Symbol, Float)

--- src/sexpconv/intrinsics.go
+++ src/sexpconv/intrinsics.go
@@ -7,11 +7,16 @@ import (
 	"sexp"
 )
 
-func (conv *Converter) intrinsic(sym string, args []ast.Expr) *sexp.Call {
+func (conv *Converter) intrinsic(sym string, args []ast.Expr) sexp.Form {
 	switch sym {
+	case "Int", "Float", "String", "Symbol":
+		// These types can be constructed only from
+		// typed values that does not require any
+		// convertion, so we ignore them.
+		return conv.Expr(args[0])
+
 	case "Call":
 		// #FIXME: non-constant symbols should also be valid.
-
 		fn := constant.StringVal(conv.valueOf(args[0]))
 		return &sexp.Call{Fn: fn, Args: conv.exprList(args[1:])}
 
