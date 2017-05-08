ebfe3ae22c38ea7a0adffebda0ee3a7f13ad447f
quasilyte <quasilyte@gmail.com>
2017-05-08 12:15:13 +0300

    lisp package for internal use (emacs/lisp is for users)

---
+++ src/lisp/lisp.go
@@ -0,0 +1,47 @@
+package lisp
+
+import (
+	"go/types"
+)
+
+type Symbol string
+
+var Package *types.Package
+
+var Typenames struct {
+	Int    *types.TypeName
+	Float  *types.TypeName
+	String *types.TypeName
+	Symbol *types.TypeName
+}
+
+var Types struct {
+	Object *types.Interface
+	Int    *types.Named
+	Float  *types.Named
+	String *types.Named
+	Symbol *types.Named
+}
+
+func Init(pkg *types.Package) {
+	top := pkg.Scope()
+	getTypename := func(name string) *types.TypeName {
+		return top.Lookup(name).(*types.TypeName)
+	}
+	getNamed := func(tn *types.TypeName) *types.Named {
+		return tn.Type().(*types.Named)
+	}
+
+	Package = pkg
+
+	Typenames.Int = getTypename("Int")
+	Types.Int = getNamed(Typenames.Int)
+	Typenames.Float = getTypename("Float")
+	Types.Float = getNamed(Typenames.Float)
+	Typenames.String = getTypename("String")
+	Types.String = getNamed(Typenames.String)
+	Typenames.Symbol = getTypename("Symbol")
+	Types.Symbol = getNamed(Typenames.Symbol)
+
+	Types.Object = top.Lookup("Object").Type().Underlying().(*types.Interface)
+}
