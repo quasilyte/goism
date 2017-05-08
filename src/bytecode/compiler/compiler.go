049ff1731bbf276cf6bc78705d18e4d9377e749e
quasilyte <quasilyte@gmail.com>
2017-05-08 18:24:26 +0300

    moving optimization outside of compiler

--- src/bytecode/compiler/compiler.go
+++ src/bytecode/compiler/compiler.go
@@ -153,15 +153,7 @@ func (cl *Compiler) compileOp(op ir.Opcode, args []sexp.Form) {
 }
 
 func (cl *Compiler) compileCall(fn lisp.Symbol, args ...sexp.Form) {
-	switch fn {
-	case "cons":
-		cl.compileInstr(ir.MakeCons(), 2, args)
-	case "car":
-		cl.compileInstr(ir.Car(), 1, args)
-	case "cdr":
-		cl.compileInstr(ir.Cdr(), 1, args)
-
-	default:
+	if !optCall(cl, fn, args) {
 		cl.emitConst(cl.constPool.InsertSym(fn))
 		cl.compileExprList(args)
 		cl.emit(ir.Call(len(args)))
@@ -274,52 +266,9 @@ func (cl *Compiler) compileConcat(form *sexp.Concat) {
 }
 
 func (cl *Compiler) compileAddSub(op ir.Opcode, args []sexp.Form) {
-	numExtract := func(x sexp.Form) int {
-		switch x := x.(type) {
-		case sexp.Int:
-			if x.Val == 1 || x.Val == 2 {
-				return int(x.Val)
-			}
-			return 0
-		case sexp.Float:
-			if x.Val == 1.0 {
-				return 1
-			} else if x.Val == 2.0 {
-				return 2
-			}
-			return 0
-		default:
-			return 0
-		}
+	if !optAddSub(cl, op, args) {
+		cl.compileOp(op, args)
 	}
-	specialize := func(num int, arg sexp.Form, instr ir.Instr) bool {
-		if num == 1 || num == 2 {
-			cl.compileExpr(arg)
-			for i := 0; i < num; i++ {
-				cl.emit(instr)
-			}
-			return true
-		}
-		return false
-	}
-	specInstr := func(op ir.Opcode) ir.Instr {
-		if op == ir.OpNumAdd {
-			return ir.NumAdd1()
-		}
-		return ir.NumSub1()
-	}
-
-	if len(args) == 2 {
-		instr := specInstr(op)
-		if specialize(numExtract(args[0]), args[1], instr) {
-			return
-		}
-		if specialize(numExtract(args[1]), args[0], instr) {
-			return
-		}
-	}
-	cl.compileOp(op, args)
-
 }
 
 func (cl *Compiler) compileExprStmt(form sexp.Form) {
