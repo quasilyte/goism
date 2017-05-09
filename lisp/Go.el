cdca86b3e7a3bdb68157d3ba603bccd253607d85
quasilyte <quasilyte@gmail.com>
2017-05-09 21:22:17 +0300

    print and println functions

--- lisp/Go.el
+++ lisp/Go.el
@@ -8,6 +8,15 @@
 ;; ----------------------
 ;; Runtime implementation
 
+(defun Go--print (&rest args)
+  (princ (mapconcat #'prin1-to-string args ""))
+  nil)
+
+(defun Go--println (&rest args)
+  (princ (mapconcat #'prin1-to-string args " "))
+  (terpri)
+  nil)
+
 (define-error 'Go--panic "Go panic")
 
 (defun Go--panic (error-data)
