f5acf8177a23221fd05c572abfcdf27a378534f7
quasilyte <quasilyte@gmail.com>
2017-05-09 18:15:13 +0300

    removing outdated files

--- elisp-runtime/debug/dump.el
+++
@@ -1,6 +0,0 @@
-(defun Go-dump-slice (slice)
-  (message "len=%d cap=%d offset=%d data=%s"
-           (Go-slice-len slice)
-           (Go-slice-cap slice)
-           (Go-slice-offset slice)
-           (Go-slice-data slice)))
--- elisp-runtime/ptr.el
+++
@@ -1,5 +0,0 @@
-;; Pointers emulation.
-
-(defmacro Go-make-ptr (val) `(cons ,val nil))
-(defmacro Go-ptr-deref (ptr) `(car ,ptr))
-(defmacro Go-ptr-set (ptr val) `(setcar ,ptr ,val))
