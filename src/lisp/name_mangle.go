bd6ee9e36116168880577b46d4b59be3ea8c3846
quasilyte <quasilyte@gmail.com>
2017-05-21 21:48:02 +0300

    fixing name mangling for lisp vars

--- src/lisp/name_mangle.go
+++ src/lisp/name_mangle.go
@@ -3,7 +3,7 @@ package lisp
 const symPrefix = "Go-"
 
 func VarName(pkgName string, varName string) string {
-	totalLen := len(symPrefix) + len(pkgName) + len("-") + len(varName)
+	totalLen := len(symPrefix) + len(pkgName) + len(".") + len(varName)
 	buf := make([]byte, totalLen)
 
 	pos := 0
@@ -11,8 +11,8 @@ func VarName(pkgName string, varName string) string {
 	pos += len(symPrefix)
 	copy(buf[pos:], pkgName)
 	pos += len(pkgName)
-	copy(buf[pos:], "-")
-	pos += len("-")
+	copy(buf[pos:], ".")
+	pos += len(".")
 	copy(buf[pos:], varName)
 
 	return string(buf)
