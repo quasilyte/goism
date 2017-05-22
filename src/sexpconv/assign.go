59fb0038df6b110691d9ccc7dc4b4464ddba2158
quasilyte <quasilyte@gmail.com>
2017-05-22 10:02:19 +0300

    blank ident in single and multiple assignments

--- src/sexpconv/assign.go
+++ src/sexpconv/assign.go
@@ -8,6 +8,19 @@ import (
 	"sexp"
 )
 
+// Go blank identifier which allows a value to be evaluated, but discarded.
+const blankIdent = "_"
+
+// This form produces no executable code. Used as NO-OP.
+var emptyForm = &sexp.FormList{}
+
+func isBlankIdent(node ast.Expr) bool {
+	if node, ok := node.(*ast.Ident); ok {
+		return node.Name == blankIdent
+	}
+	return false
+}
+
 func (conv *Converter) AssignStmt(node *ast.AssignStmt) sexp.Form {
 	switch node.Tok {
 	case token.ADD_ASSIGN:
@@ -65,13 +78,25 @@ func (conv *Converter) quoAssign(lhs ast.Expr, rhs ast.Expr) sexp.Form {
 func (conv *Converter) multiValueAssign(node *ast.AssignStmt) *sexp.FormList {
 	forms := make([]sexp.Form, len(node.Lhs))
 
+	tuple := conv.typeOf(node.Rhs[0]).(*types.Tuple)
+
 	// First result is returned in a normal way.
-	forms[0] = conv.assign(node.Lhs[0], conv.Expr(node.Rhs[0]))
+	if isBlankIdent(node.Lhs[0]) {
+		// Do a call, but discard the return value.
+		forms[0] = sexp.CallStmt{
+			Call: conv.Expr(node.Rhs[0]).(*sexp.Call),
+		}
+	} else {
+		forms[0] = conv.assign(node.Lhs[0], conv.Expr(node.Rhs[0]))
+	}
 
 	// Other results are assigned to a global variable.
 	// Index uniquely identifies variable used for storage.
 	for i := 1; i < len(node.Lhs); i++ {
-		forms[i] = conv.assign(node.Lhs[i], &sexp.MultiValueRef{Index: i})
+		forms[i] = conv.assign(node.Lhs[i], &sexp.MultiValueRef{
+			Index: i,
+			Typ:   tuple.At(i).Type(),
+		})
 	}
 
 	return &sexp.FormList{Forms: forms}
@@ -92,6 +117,9 @@ func (conv *Converter) singleValueAssign(node *ast.AssignStmt) *sexp.FormList {
 func (conv *Converter) assign(lhs ast.Expr, expr sexp.Form) sexp.Form {
 	switch lhs := lhs.(type) {
 	case *ast.Ident:
+		if lhs.Name == blankIdent {
+			return conv.ignoredExpr(expr)
+		}
 		if def := conv.info.Defs[lhs]; def == nil {
 			return &sexp.Rebind{Name: lhs.Name, Expr: expr}
 		}
@@ -124,5 +152,18 @@ func (conv *Converter) assign(lhs ast.Expr, expr sexp.Form) sexp.Form {
 	// #TODO: struct assign, indirect assign
 	default:
 		panic("unimplemented")
+	}
+}
+
+func (conv *Converter) ignoredExpr(expr sexp.Form) sexp.Form {
+	switch expr := expr.(type) {
+	case *sexp.Call:
+		// Function call can not be ignored because
+		// it may have side effects.
+		return sexp.CallStmt{Call: expr}
+
+	default:
+		// Ignored completely.
+		return emptyForm
 	}
 }
