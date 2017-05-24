47e52e0362cf134c551f9fbee906b346b71279d3
quasilyte <quasilyte@gmail.com>
2017-05-24 09:51:57 +0300

    let helpers for sexpconv

--- src/sexpconv/constructors.go
+++ src/sexpconv/constructors.go
@@ -1,8 +1,26 @@
 package sexpconv
 
 import (
+	"go/types"
 	"sexp"
 )
+
+func _it(typ types.Type) sexp.Var {
+	return sexp.Var{Name: "_it", Typ: typ}
+}
+
+func _let(val sexp.Form, form sexp.Form) *sexp.Let {
+	if sexp.IsStmt(form) {
+		return &sexp.Let{
+			Bind: &sexp.Bind{Name: "_it", Init: val},
+			Stmt: form,
+		}
+	}
+	return &sexp.Let{
+		Bind: &sexp.Bind{Name: "_it", Init: val},
+		Expr: form,
+	}
+}
 
 func _numLt(a, b sexp.Form) *sexp.NumLt {
 	return &sexp.NumLt{Args: [2]sexp.Form{a, b}}
