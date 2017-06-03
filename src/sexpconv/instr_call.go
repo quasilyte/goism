6fa1afd75315b1a483925b7288d8f35db6aac9eb
quasilyte <quasilyte@gmail.com>
2017-06-03 22:20:50 +0300

    recognizing concat lisp call

--- src/sexpconv/instr_call.go
+++ src/sexpconv/instr_call.go
@@ -5,25 +5,32 @@ import "sexp"
 import "go/ast"
 
 var nameToInstr = map[string]instr.Instr{
-	"cons":    instr.Cons,
-	"car":     instr.Car,
-	"cdr":     instr.Car,
-	"aref":    instr.ArrayRef,
-	"aset":    instr.ArraySet,
-	"=":       instr.NumEq,
-	">":       instr.NumGt,
-	"<":       instr.NumLt,
-	"<=":      instr.NumLte,
-	">=":      instr.NumGte,
-	"+":       instr.NumAdd,
-	"-":       instr.NumSub,
-	"*":       instr.NumMul,
-	"/":       instr.NumQuo,
-	"string=": instr.StrEq,
-	"string<": instr.StrLt,
+	"cons":      instr.Cons,
+	"car":       instr.Car,
+	"cdr":       instr.Car,
+	"aref":      instr.ArrayRef,
+	"aset":      instr.ArraySet,
+	"=":         instr.NumEq,
+	">":         instr.NumGt,
+	"<":         instr.NumLt,
+	"<=":        instr.NumLte,
+	">=":        instr.NumGte,
+	"+":         instr.NumAdd,
+	"-":         instr.NumSub,
+	"*":         instr.NumMul,
+	"/":         instr.NumQuo,
+	"string=":   instr.StrEq,
+	"string<":   instr.StrLt,
+	"substring": instr.Substr,
 }
 
 func (conv *Converter) instrCall(name string, args []ast.Expr) *sexp.InstrCall {
+	if name == "concat" {
+		return &sexp.InstrCall{
+			Instr: instr.Concat(len(args)),
+			Args:  conv.valueCopyList(conv.exprList(args)),
+		}
+	}
 	ins, ok := nameToInstr[name]
 	if !ok {
 		return nil
