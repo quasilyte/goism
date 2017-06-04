8725aa7579531fb73f97f292bf36a4b3204cfc5f
quasilyte <quasilyte@gmail.com>
2017-06-04 14:44:15 +0300

    Go-rt.panic in ThrowingFuncs slice

--- src/lisp/rt/rt.go
+++ src/lisp/rt/rt.go
@@ -14,8 +14,9 @@ var RetVars = [...]string{
 }
 
 var ThrowingFuncs = map[string]bool{
-	"Go--panic": true,
-	"error":     true,
-	"throw":     true,
-	"signal":    true,
+	"Go--panic":   true,
+	"Go-rt.panic": true,
+	"error":       true,
+	"throw":       true,
+	"signal":      true,
 }
