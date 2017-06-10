5e7c8f6742d07d2e34684ea475f70ecbc9c3d3e3
quasilyte <quasilyte@gmail.com>
2017-06-10 13:59:48 +0300

    now it is only legal to import packages with 'emacs/' prefix

--- src/tu/load/importer.go
+++ src/tu/load/importer.go
@@ -1,8 +1,10 @@
 package load
 
 import (
+	"errors"
 	"go/types"
 	"magic_pkg/emacs/lisp"
+	"strings"
 )
 
 type emacsImporter struct {
@@ -10,6 +12,10 @@ type emacsImporter struct {
 }
 
 func (ei *emacsImporter) Import(path string) (*types.Package, error) {
+	if !strings.HasPrefix(path, "emacs/") {
+		return nil, errors.New("missing `emacs/' package path prefix")
+	}
+
 	pkg, err := ei.impl.Import(path)
 	if path == "emacs/lisp" && err == nil && lisp.Package == nil {
 		return pkg, lisp.InitPackage(pkg)
