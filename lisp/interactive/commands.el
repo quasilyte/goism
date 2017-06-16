46c56fb5b3b7837865843739be499ced06eaea38
quasilyte <quasilyte@gmail.com>
2017-06-16 11:42:46 +0300

    updating lisp/interactive code

--- lisp/interactive/commands.el
+++ lisp/interactive/commands.el
@@ -2,71 +2,62 @@
 ;; Interactive commands.
 
 (eval-when-compile
-  (defmacro Go--cmd-exit-code (res)
+  (defmacro goism--cmd-exit-code (res)
     `(car ,res))
-  (defmacro Go--cmd-output (res)
+  (defmacro goism--cmd-output (res)
     `(cdr ,res)))
 
-(defun Go-translate-by-path (pkg-path)
-  "Read Go package located at PKG-PATH and translate it into Emacs Lisp.
+(defun goism-translate (pkg-path)
+  "Read Go package PKG-PATH and translate it into Emacs Lisp package.
+PKG-PATH should be prefixed with `emacs/'.
 Generated code is shown in temporary buffer.
 Note that this method depends on GOPATH environment variables.
-Requires `goel_translate_package' to be available."
-  (interactive "DGo package path: ")
-  (let* ((pkg-path (expand-file-name pkg-path))
-         (res (Go--exec
+Requires `goism_translate_package' to be available.
+
+Example: `(goism-translate \"emacs/example\")'"
+  (interactive "sGo package: ")
+  (let* ((res (goism--exec
                "goel_translate_package"
                (format "-pkgPath=%s" pkg-path))))
-    (Go--ir-pkg-compile (read (Go--cmd-output res)))))
+    (Go--ir-pkg-compile (read (goism--cmd-output res)))))
 
-(defun Go-translate-by-name (pkg-name)
-  "Like `Go-translate-by-path', but prepends `Go-emacs-gopath'
-to specified package name."
-  (interactive "sGo package name: ")
-  (Go-translate-by-path (concat Go-emacs-gopath "/src/emacs/" pkg-name)))
-
-(defun Go-load-by-name (pkg-name)
-  "Calls `Go-translate-by-name', evaluates output buffer and then closes it.
+(defun goism-load (pkg-path)
+  "Calls `goism-translate', evaluates output buffer and then closes it.
 Not recommended for untrusted packages."
-  (interactive "sGo package name: ")
-  (Go-translate-by-name pkg-name)
-  (with-current-buffer Go-output-buffer-name
+  (interactive "sGo package: ")
+  (goism-translate-by-name pkg-path)
+  (with-current-buffer goism-output-buffer-name
     (eval-buffer)
     (kill-buffer-and-window)))
 
-(defun Go-disassemble-by-path (pkg-path &optional disable-opt)
-  "Read Go package located at PKG-PATH and print its IR.
+
+
+(defun goism-disassemble (pkg-path &optional disable-opt)
+  "Read Go package PKG-PATH and print its IR.
 Output is shown in temporary buffer.
-Requires `goel_translate_package' to be available."
+Requires `goism_translate_package' to be available."
   (interactive "DGo package path: ")
-  (let* ((pkg-path (expand-file-name pkg-path))
-         (opt-arg (if disable-opt "false" "true"))
-         (res (Go--exec
+  (let* ((opt-arg (if disable-opt "false" "true"))
+         (res (goism--exec
                "goel_translate_package"
                "-output=asm"
                (format "-pkgPath=%s" pkg-path)
                (format "-opt=%s" opt-arg))))
-    (with-output-to-temp-buffer Go-output-buffer-name
-      (princ (Go--cmd-output res)))))
+    (with-output-to-temp-buffer goism-output-buffer-name
+      (princ (goism--cmd-output res)))))
 
-(defun Go-disassemble-by-name (pkg-name)
-  "Like `Go-disassemble-by-path', but prepends `Go-emacs-package-path'
-to specified package name."
-  (interactive "sGo package name: ")
-  (Go-disassemble-by-path (concat Go-emacs-gopath "/src/emacs/" pkg-name)))
-
-(defun Go--exec (cmd &rest args)
-  (let* ((cmd (if (string= "" Go-utils-path)
+(defun goism--exec (cmd &rest args)
+  (let* ((cmd (if (string= "" goism-utils-path)
                   cmd
-                (format "%s/%s" Go-utils-path cmd)))
-         (env (cons (format "GOPATH=%s" (expand-file-name Go-emacs-gopath))
+                (format "%s/%s" goism-utils-path cmd)))
+         (env (cons (format "GOPATH=%s" (expand-file-name goism-emacs-gopath))
                     process-environment))
          (res (with-temp-buffer
                 (let ((process-environment env))
                   (cons (apply #'call-process cmd nil t nil args)
                         (buffer-string))))))
-    (when (/= 0 (Go--cmd-exit-code res))
-      (error (Go--cmd-output res)))
+    (when (/= 0 (goism--cmd-exit-code res))
+      (error (goism--cmd-output res)))
     res))
 
 ;; {{ end }}
--- lisp/interactive/customization.el
+++ lisp/interactive/customization.el
@@ -5,18 +5,18 @@
   "Configure Go->Emacs Lisp translator."
   :group 'development)
 
-(defcustom Go-utils-path ""
+(defcustom goism-utils-path ""
   "Specifies path which is used to find required binaries.
 If remain empty, system PATH is used."
   :group 'goism
   :type 'directory)
 
-(defcustom Go-emacs-gopath "~/.emacs.d/goism"
+(defcustom goism-emacs-gopath "~/.emacs.d/goism"
   "GOPATH used for Emacs Go packages."
   :group 'goism
   :type 'directory)
 
-(defcustom Go-output-buffer-name "*goisl compile*"
+(defcustom goism-output-buffer-name "*goisl compile*"
   "Temporary buffer name that is used for output."
   :group 'goism
   :type 'buffer-name)
