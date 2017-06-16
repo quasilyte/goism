df77cc306e0b7461ddeac0ee2b472c7f9e94c6df
quasilyte <quasilyte@gmail.com>
2017-06-16 11:55:58 +0300

    simplifying emacs/example package

--- src/emacs/example/example.go
+++ src/emacs/example/example.go
@@ -6,14 +6,7 @@ import (
 	"emacs/lisp"
 )
 
-// PrintMessage prints given message using `message` Lisp function.
-func PrintMessage(msg string) {
-	lisp.Call("message", lisp.Str(msg))
-}
-
-// PrintFiveLetters prints 5 letters using `println' builtin.
-func PrintFiveLetters(letters [5]rune) {
-	for i := range letters {
-		println(letters[i])
-	}
+// HelloEmacs prints "Hello, world!" using `message' Lisp function.
+func HelloEmacs() {
+	lisp.Message("Hello, world!")
 }
