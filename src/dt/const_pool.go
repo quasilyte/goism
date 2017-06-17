72af05e33aa916f57e045a126677d2c474e858e0
quasilyte <quasilyte@gmail.com>
2017-06-17 10:42:12 +0300

    documentation for dt.ConstPool

--- src/dt/const_pool.go
+++ src/dt/const_pool.go
@@ -6,14 +6,21 @@ import (
 	"strconv"
 )
 
+// ConstPool is a set of distincs constant values.
+// It stores atoms of int, float, string and symbol types.
+//
+// Serves as a builder for Emacs function constant vector.
 type ConstPool struct {
 	vals []interface{}
 }
 
+// Clear removes all stored elements. Storage is reused.
 func (cp *ConstPool) Clear() {
 	cp.vals = cp.vals[:0]
 }
 
+// InsertInt inserts given argument if it is not already present.
+// Returns constant vector index.
 func (cp *ConstPool) InsertInt(x int64) int {
 	for i, val := range cp.vals {
 		if val == x {
@@ -25,6 +32,8 @@ func (cp *ConstPool) InsertInt(x int64) int {
 	return len(cp.vals) - 1
 }
 
+// InsertFloat inserts given argument if it is not already present.
+// Returns constant vector index.
 func (cp *ConstPool) InsertFloat(x float64) int {
 	for i, val := range cp.vals {
 		if val == x {
@@ -36,6 +45,8 @@ func (cp *ConstPool) InsertFloat(x float64) int {
 	return len(cp.vals) - 1
 }
 
+// InsertString inserts given argument if it is not already present.
+// Returns constant vector index.
 func (cp *ConstPool) InsertString(x string) int {
 	for i, val := range cp.vals {
 		if val == x {
@@ -47,6 +58,8 @@ func (cp *ConstPool) InsertString(x string) int {
 	return len(cp.vals) - 1
 }
 
+// InsertSym inserts given argument if it is not already present.
+// Returns constant vector index.
 func (cp *ConstPool) InsertSym(x string) int {
 	sym := lisp.Symbol(x)
 	for i, val := range cp.vals {
@@ -59,26 +72,32 @@ func (cp *ConstPool) InsertSym(x string) int {
 	return len(cp.vals) - 1
 }
 
+// Get extracts constant vector value stored at specified index.
 func (cp *ConstPool) Get(index uint16) interface{} {
 	return cp.vals[index]
 }
 
+// GetInt is like Get, but result is type asserted to int64.
 func (cp *ConstPool) GetInt(index uint16) int64 {
 	return cp.vals[index].(int64)
 }
 
+// GetFloat is like Get, but result is type asserted to float64.
 func (cp *ConstPool) GetFloat(index uint16) float64 {
 	return cp.vals[index].(float64)
 }
 
+// GetString is like Get, but result is type asserted to string.
 func (cp *ConstPool) GetString(index uint16) string {
 	return cp.vals[index].(string)
 }
 
+// GetSym is like Get, but result is type asserted to lisp.Symbol.
 func (cp *ConstPool) GetSym(index int) lisp.Symbol {
 	return cp.vals[index].(lisp.Symbol)
 }
 
+// Bytes returns printed representation of Emacs Lisp constant vector.
 func (cp *ConstPool) Bytes() []byte {
 	buf := bytes.Buffer{}
 	buf.WriteByte('[')
