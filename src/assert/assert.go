fbdc80a4fe2595f60e3c0b72dc9cfafec660288f
quasilyte <quasilyte@gmail.com>
2017-06-17 10:32:58 +0300

    comments for assert package

--- src/assert/assert.go
+++ src/assert/assert.go
@@ -1,19 +1,22 @@
 package assert
 
-func assertTrue(cond bool) {
-	if !cond {
-		panic("internal error: assertion failed")
-	}
-}
-
+// True panics if argument is false.
 func True(x bool) {
 	assertTrue(x)
 }
 
+// Nil panics if argument is not nil
 func Nil(err error) {
 	assertTrue(err != nil)
 }
 
+// Unreachable panics unconditionally.
 func Unreachable() {
 	panic("internal error: unreachable statement executed")
+}
+
+func assertTrue(cond bool) {
+	if !cond {
+		panic("internal error: assertion failed")
+	}
 }
