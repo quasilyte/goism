3d45d4e9622fc7e8eed1bfd7d458d2280c9539c1
quasilyte <quasilyte@gmail.com>
2017-06-20 12:03:14 +0300

    refs #60 - comparatorEq function

--- src/sexpconv/utils.go
+++ src/sexpconv/utils.go
@@ -2,6 +2,8 @@ package sexpconv
 
 import (
 	"go/ast"
+	"go/types"
+	"ir/instr"
 	"sexp"
 )
 
@@ -40,4 +42,30 @@ func toFormList(form sexp.Form) *sexp.FormList {
 		return form
 	}
 	return &sexp.FormList{Forms: []sexp.Form{form}}
+}
+
+// Returns a form which is a equallity comparator for two given forms.
+// Returns nil when comparison over {"a", "b"} is undefined (or unimplemented).
+func comparatorEq(a, b sexp.Form) sexp.Form {
+	switch typ := a.Type(); typ := typ.(type) {
+	case *types.Basic:
+		if typ.Info()&types.IsNumeric != 0 {
+			return sexp.NewNumEq(a, b)
+		} else if typ.Kind() == types.String {
+			return sexp.NewStrEq(a, b)
+		}
+		return nil
+
+	case *types.Named:
+		// #REFS: 60.
+		return nil
+
+	default:
+		// Fallback to "eq" comparison.
+		// Should work for pointer comparisons.
+		return &sexp.InstrCall{
+			Instr: instr.Eq,
+			Args:  []sexp.Form{a, b},
+		}
+	}
 }
