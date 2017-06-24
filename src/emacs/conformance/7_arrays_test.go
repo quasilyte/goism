5a8e872788c3d6a7da475bb830d2ae393172fc60
quasilyte <quasilyte@gmail.com>
2017-06-24 09:39:09 +0300

    refs #62 - basic arrays tests

--- src/emacs/conformance/7_arrays_test.go
+++ src/emacs/conformance/7_arrays_test.go
@@ -1,3 +1,68 @@
 package conformance
 
 // #REFS: 5.
+
+func testArrayLit(n int) int {
+	xs := [...]int{n, n + 1, n + 2}
+	if xs[0] == n && xs[1] == n+1 && xs[2] == n+2 {
+		return n
+	}
+	return -1
+}
+
+// #REFS: 73.
+// func testKeyedArrayLit(n int) int {
+// 	xs := [...]int{
+// 		1: n,
+// 		3: n + 1,
+// 	}
+// 	switch {
+// 	case len(xs) != 4:
+// 		return -1
+// 	case xs[0] != 0:
+// 		return -1
+// 	case xs[1] != n:
+// 		return -1
+// 	case xs[2] != 0:
+// 		return -1
+// 	case xs[3] != n+1:
+// 		return -1
+// 	default:
+// 		return n
+// 	}
+// }
+
+func testArrayZeroVal(n int) int {
+	var xs [2]float64
+	var ys [2]int
+	var zs [2]string
+	switch {
+	case !(xs[0] == 0.0 && xs[1] == 0.0):
+		return -1
+	case !(ys[0] == 0 && ys[1] == 0):
+		return -1
+	case !(zs[0] == "" && zs[1] == ""):
+		return -1
+	default:
+		return n
+	}
+}
+
+func testArrayUpdate(n int) int {
+	var xs [3]int
+	xs[0], xs[2] = n, n+1
+	if xs[0] == n && xs[1] == 0 && xs[2] == n+1 {
+		return n
+	}
+	return -1
+}
+
+func testArrayCopyOnAssign(n int) int {
+	xs := [...]int{n, n * 5}
+	ys := xs
+	ys[0], ys[1] = 0, 1
+	if xs[0] != n || xs[1] != n*5 {
+		return -1
+	}
+	return n
+}
--- src/tst/conformance/conformance_test.go
+++ src/tst/conformance/conformance_test.go
@@ -183,6 +183,16 @@ func Test5If(t *testing.T) {
 
 func Test6Switch(t *testing.T) {
 	testCalls(t, []callTest{
+		{"testArrayLit 10", "10"},
+		// {"testKeyedArrayLit 10", "10"}, #REFS: 73
+		{"testArrayZeroVal 10", "10"},
+		{"testArrayUpdate 10", "10"},
+		{"testArrayCopyOnAssign 10", "10"},
+	})
+}
+
+func Test7Arrays(t *testing.T) {
+	testCalls(t, []callTest{
 		{"stringifyInt3 0", `"0"`},
 		{"stringifyInt3 1", `"1"`},
 		{"stringifyInt3 2", `"2"`},
@@ -192,10 +202,6 @@ func Test6Switch(t *testing.T) {
 		{"stringifyInt4 2", `"2"`},
 		{"stringifyInt4 3", `"x"`},
 	})
-}
-
-func Test7Arrays(t *testing.T) {
-
 }
 
 func Test8Slices(t *testing.T) {
