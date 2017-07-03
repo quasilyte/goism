0f5f5acc31330ed3f4aeb21a0baded9074806c08
quasilyte <quasilyte@gmail.com>
2017-07-04 02:17:16 +0300

    s/f/fn

--- src/sexp/rewrite.go
+++ src/sexp/rewrite.go
@@ -3,147 +3,147 @@ package sexp
 type rewriteFunc func(Form) Form
 
 // Rewrite provides convenient way to update AST.
-func Rewrite(form Form, f rewriteFunc) Form {
+func Rewrite(form Form, fn rewriteFunc) Form {
 	switch form := form.(type) {
 	case *Block:
-		return rewriteList(form, form.Forms, f)
+		return rewriteList(form, form.Forms, fn)
 	case *FormList:
-		return rewriteList(form, form.Forms, f)
+		return rewriteList(form, form.Forms, fn)
 	case *ExprStmt:
-		return rewrite(form, f, &form.Expr)
+		return rewrite(form, fn, &form.Expr)
 	case Bool:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case Int:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case Float:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case Str:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case Symbol:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case Var:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case *ArrayLit:
-		return rewriteList(form, form.Vals, f)
+		return rewriteList(form, form.Vals, fn)
 
 	case *Goto:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	case *Label:
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 
 	case *SparseArrayLit:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
 		for i := range form.Vals {
-			form.Vals[i] = Rewrite(form.Vals[i], f)
+			form.Vals[i] = Rewrite(form.Vals[i], fn)
 		}
 
 	case *SliceLit:
-		return rewriteList(form, form.Vals, f)
+		return rewriteList(form, form.Vals, fn)
 	case *ArrayIndex:
-		return rewrite(form, f, &form.Array, &form.Index)
+		return rewrite(form, fn, &form.Array, &form.Index)
 	case *ArrayUpdate:
-		return rewrite(form, f, &form.Array, &form.Index, &form.Expr)
+		return rewrite(form, fn, &form.Array, &form.Index, &form.Expr)
 	case *ArraySlice:
-		return rewriteSpan(form, &form.Array, &form.Span, f)
+		return rewriteSpan(form, &form.Array, &form.Span, fn)
 	case *SliceIndex:
-		return rewrite(form, f, &form.Slice, &form.Index)
+		return rewrite(form, fn, &form.Slice, &form.Index)
 	case *SliceUpdate:
-		return rewrite(form, f, &form.Slice, &form.Index, &form.Expr)
+		return rewrite(form, fn, &form.Slice, &form.Index, &form.Expr)
 	case *SliceSlice:
-		return rewriteSpan(form, &form.Slice, &form.Span, f)
+		return rewriteSpan(form, &form.Slice, &form.Span, fn)
 	case *Bind:
-		return rewrite(form, f, &form.Init)
+		return rewrite(form, fn, &form.Init)
 	case *Rebind:
-		return rewrite(form, f, &form.Expr)
+		return rewrite(form, fn, &form.Expr)
 	case *VarUpdate:
-		return rewrite(form, f, &form.Expr)
+		return rewrite(form, fn, &form.Expr)
 	case *TypeAssert:
-		return rewrite(form, f, &form.Expr)
+		return rewrite(form, fn, &form.Expr)
 
 	case *If:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Cond = Rewrite(form.Cond, f)
-		form.Then = Rewrite(form.Then, f).(*Block)
-		form.Else = Rewrite(form.Else, f)
+		form.Cond = Rewrite(form.Cond, fn)
+		form.Then = Rewrite(form.Then, fn).(*Block)
+		form.Else = Rewrite(form.Else, fn)
 
 	case *Switch:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Expr = Rewrite(form.Expr, f)
-		form.SwitchBody = rewriteSwitchBody(form.SwitchBody, f)
+		form.Expr = Rewrite(form.Expr, fn)
+		form.SwitchBody = rewriteSwitchBody(form.SwitchBody, fn)
 
 	case *SwitchTrue:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.SwitchBody = rewriteSwitchBody(form.SwitchBody, f)
+		form.SwitchBody = rewriteSwitchBody(form.SwitchBody, fn)
 
 	case *Return:
-		return rewriteList(form, form.Results, f)
+		return rewriteList(form, form.Results, fn)
 
 	case *Repeat:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Body = Rewrite(form.Body, f).(*Block)
+		form.Body = Rewrite(form.Body, fn).(*Block)
 
 	case *DoTimes:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Step = Rewrite(form.Step, f)
-		form.Body = Rewrite(form.Body, f).(*Block)
+		form.Step = Rewrite(form.Step, fn)
+		form.Body = Rewrite(form.Body, fn).(*Block)
 
 	case *Loop:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Body = Rewrite(form.Body, f).(*Block)
+		form.Body = Rewrite(form.Body, fn).(*Block)
 
 	case *While:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
-		form.Cond = Rewrite(form.Cond, f)
-		form.Post = Rewrite(form.Post, f)
-		form.Body = Rewrite(form.Body, f).(*Block)
+		form.Cond = Rewrite(form.Cond, fn)
+		form.Post = Rewrite(form.Post, fn)
+		form.Body = Rewrite(form.Body, fn).(*Block)
 
 	case *LispCall:
-		return rewriteList(form, form.Args, f)
+		return rewriteList(form, form.Args, fn)
 	case *Call:
-		return rewriteList(form, form.Args, f)
+		return rewriteList(form, form.Args, fn)
 
 	case *Let:
-		if form := f(form); form != nil {
+		if form := fn(form); form != nil {
 			return form
 		}
 		for i, bind := range form.Bindings {
-			form.Bindings[i] = Rewrite(bind, f).(*Bind)
+			form.Bindings[i] = Rewrite(bind, fn).(*Bind)
 		}
 		if form.Expr == nil {
-			form.Stmt = Rewrite(form.Stmt, f)
+			form.Stmt = Rewrite(form.Stmt, fn)
 		} else {
-			form.Expr = Rewrite(form.Expr, f)
+			form.Expr = Rewrite(form.Expr, fn)
 		}
 
 	case *TypeCast:
-		return rewrite(form, f, &form.Form)
+		return rewrite(form, fn, &form.Form)
 
 	case *StructLit:
-		return rewriteList(form, form.Vals, f)
+		return rewriteList(form, form.Vals, fn)
 	case *StructIndex:
-		return rewrite(form, f, &form.Struct)
+		return rewrite(form, fn, &form.Struct)
 	case *StructUpdate:
-		return rewrite(form, f, &form.Struct, &form.Expr)
+		return rewrite(form, fn, &form.Struct, &form.Expr)
 	case *And:
-		return rewrite(form, f, &form.X, &form.Y)
+		return rewrite(form, fn, &form.X, &form.Y)
 	case *Or:
-		return rewrite(form, f, &form.X, &form.Y)
+		return rewrite(form, fn, &form.X, &form.Y)
 
 	case nil:
 		return form
@@ -153,59 +153,59 @@ func Rewrite(form Form, f rewriteFunc) Form {
 
 	default:
 		// Can be backend-specific form.
-		return rewriteAtom(form, f)
+		return rewriteAtom(form, fn)
 	}
 
 	return form
 }
 
-func rewriteSwitchBody(b SwitchBody, f rewriteFunc) SwitchBody {
+func rewriteSwitchBody(b SwitchBody, fn rewriteFunc) SwitchBody {
 	for i, cc := range b.Clauses {
 		b.Clauses[i] = CaseClause{
-			Expr: Rewrite(cc.Expr, f),
-			Body: Rewrite(cc.Body, f).(*Block),
+			Expr: Rewrite(cc.Expr, fn),
+			Body: Rewrite(cc.Body, fn).(*Block),
 		}
 	}
-	b.DefaultBody = Rewrite(b.DefaultBody, f).(*Block)
+	b.DefaultBody = Rewrite(b.DefaultBody, fn).(*Block)
 	return b
 }
 
-func rewriteSpan(form Form, container *Form, span *Span, f rewriteFunc) Form {
-	if form := f(form); form != nil {
+func rewriteSpan(form Form, container *Form, span *Span, fn rewriteFunc) Form {
+	if form := fn(form); form != nil {
 		return form
 	}
-	*container = Rewrite(*container, f)
+	*container = Rewrite(*container, fn)
 	if span.Low != nil {
-		span.Low = Rewrite(span.Low, f)
+		span.Low = Rewrite(span.Low, fn)
 	}
 	if span.High != nil {
-		span.High = Rewrite(span.High, f)
+		span.High = Rewrite(span.High, fn)
 	}
 	return form
 }
 
-func rewriteList(form Form, forms []Form, f rewriteFunc) Form {
-	if form := f(form); form != nil {
+func rewriteList(form Form, forms []Form, fn rewriteFunc) Form {
+	if form := fn(form); form != nil {
 		return form
 	}
 	for i, form := range forms {
-		forms[i] = Rewrite(form, f)
+		forms[i] = Rewrite(form, fn)
 	}
 	return form
 }
 
-func rewrite(form Form, f rewriteFunc, xs ...*Form) Form {
-	if form := f(form); form != nil {
+func rewrite(form Form, fn rewriteFunc, xs ...*Form) Form {
+	if form := fn(form); form != nil {
 		return form
 	}
 	for _, x := range xs {
-		*x = Rewrite(*x, f)
+		*x = Rewrite(*x, fn)
 	}
 	return form
 }
 
-func rewriteAtom(form Form, f rewriteFunc) Form {
-	if form := f(form); form != nil {
+func rewriteAtom(form Form, fn rewriteFunc) Form {
+	if form := fn(form); form != nil {
 		return form
 	}
 	return form
