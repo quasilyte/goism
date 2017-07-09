c814d8a209c53778e1a1ba3cbeb4d8a8dc440fb8
quasilyte <quasilyte@gmail.com>
2017-07-09 18:23:44 +0300

    using new "noinline" directive in emacs/rt

--- src/emacs/rt/builtin.go
+++ src/emacs/rt/builtin.go
@@ -5,20 +5,21 @@ import (
 )
 
 // Panic triggers run-time panic.
+//goism:noinline
 func Panic(errorData lisp.Object) {
-	{
-	} // Prevent inlining (#REFS: 37)
 	lisp.Call("signal", lisp.Intern("error"), lisp.Call("list", errorData))
 }
 
 // Print prints all arguments;
 // formatting of arguments is implementation-specific.
+//goism:noinline
 func Print(args lisp.Object) {
 	lisp.Call("princ", lisp.MapConcat(lisp.Prin1ToString, args, ""))
 }
 
 // Println is like print but prints spaces between arguments
 // and a newline at the end.
+//goism:noinline
 func Println(args lisp.Object) {
 	lisp.Call("princ", lisp.MapConcat(lisp.Prin1ToString, args, " "))
 	lisp.Call("terpri") // Better than princ("\n") due to spared "\n"
